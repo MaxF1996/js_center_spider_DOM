b810c7cf7400888553cb467f4e7fbc2ded5e38c2
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-11 05:20:32 +0300

    make build

---
+++ index.896e86e6.js
@@ -0,0 +1,2 @@
+var e,t,c,l,i=document.querySelector(".wall"),n=document.querySelector(".spider");e=i.clientWidth/2,t=i.clientHeight/2,c=n.clientWidth/2,l=n.clientHeight/2,n.style.left="".concat(e-c,"px"),n.style.top="".concat(t-l,"px");
+//# sourceMappingURL=index.896e86e6.js.map
